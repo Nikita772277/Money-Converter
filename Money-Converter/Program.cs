32b5889f015a85c6a36ead66367ecceace31910c
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-11-22 11:58:35 +0300

    Добавьте файлы проекта.

---
+++ Money-Converter/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Money_Converter;
+
+
+//Yuan yuan = new (){ Money = 123 };
+//Euro yuan2 = new() { Money = 123 };
+//Euro euro = (Euro) yuan;//Евро в юань
+//Yuan euro2 = (Yuan) yuan2;//Юань в евро
+//Console.WriteLine(euro.Money);
+//Console.WriteLine(euro2.Money);
+
+
+Fraction fraction = new(1, 1234);
+Fraction fraction1 = + fraction;
+Console.WriteLine(fraction1);
---
+++ Money-Converter/Yuan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Converter
+{
+    internal class Yuan
+    {
+        public decimal Money  { get; set; }
+
+        public static implicit operator Yuan(Euro x)
+        {
+
+            return new Yuan { Money = x.Money * (decimal) 2.0 };
+        }
+        public static explicit operator Euro(Yuan counter)
+        {
+
+            return new Euro { Money=counter.Money / (decimal)1.15 };
+        }
+        public void Summ()
+        {
+            
+            Console.WriteLine();
+        }
+    }
+}
