16364fb802517187079147fe89ad17c22a162328
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-11-28 13:00:04 +0300

    123

---
+++ Money-Converter/MyNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Converter
+{
+    class MyNumber
+    {
+        public int Value { get; set; }
+
+        public static MyNumber operator +(MyNumber counter1, MyNumber counter2)
+        {
+            return new MyNumber { Value = counter1.Value + counter2.Value };
+        }
+        public static MyNumber operator *(MyNumber counter1, MyNumber counter2)
+        {
+            return new MyNumber { Value = counter1.Value * counter2.Value };
+        }
+        public static MyNumber operator -(MyNumber counter1, MyNumber counter2)
+        {
+            return new MyNumber() { Value = counter1.Value - counter2.Value };
+        }
+        public static MyNumber operator /(MyNumber counter1, MyNumber counter2)
+        {
+            return new MyNumber() { Value = counter1.Value / counter2.Value };
+        }
+    }
+}
--- Money-Converter/Program.cs
+++ Money-Converter/Program.cs
@@ -1,15 +1,71 @@
-﻿
-using Money_Converter;
+﻿using Money_Converter;
+using System.Diagnostics.Metrics;
 
 
-//Yuan yuan = new (){ Money = 123 };
-//Euro yuan2 = new() { Money = 123 };
-//Euro euro = (Euro) yuan;//Евро в юань
-//Yuan euro2 = (Yuan) yuan2;//Юань в евро
-//Console.WriteLine(euro.Money);
-//Console.WriteLine(euro2.Money);
-
-
-Fraction fraction = new(1, 1234);
-Fraction fraction1 = + fraction;
-Console.WriteLine(fraction1);
+void Menu()
+{
+    Console.WriteLine();
+    Console.WriteLine($"1) Перевод Евро в юань");
+    Console.WriteLine($"2) Превод Юань в Евро");
+    Console.WriteLine($"3) Все перегрузки операторов");
+    Console.WriteLine();
+}
+void GetMenu()
+{
+    while (true)
+    {
+        Menu();
+        string enter = Console.ReadLine();
+        bool convert = int.TryParse(enter, out var result);
+        if (result == 1)
+        {
+            Console.WriteLine($"Введите число евро чтобы узнать сколько получится при переводе");
+            string moneu = Console.ReadLine();
+            Console.WriteLine($"Введите число юаней");
+            string moneu2 = Console.ReadLine();
+            bool check = int.TryParse(moneu, out var numbereuros);
+            bool check2 = int.TryParse(moneu2, out var numberyuans);
+            Yuan yuan1 = new() { Money = numbereuros };
+            Euro yuan2 = new() { Money = numberyuans };
+            Euro euro = (Euro)yuan1;
+            Console.WriteLine(euro.Money);           
+        }
+        else if (result == 2)
+        {
+            Console.WriteLine($"Введите число юаней чтобы узнать сколько получится при переводе");
+            string moneu = Console.ReadLine();
+            Console.WriteLine($"Введите число евро");
+            string moneu2 = Console.ReadLine();
+            bool check = int.TryParse(moneu, out var numbereuros);
+            bool check2 = int.TryParse(moneu2, out var numberyuans);
+            Yuan yuan1 = new() { Money = numberyuans };
+            Euro yuan2 = new() { Money = numbereuros };
+            Yuan euro2 = (Yuan)yuan2;
+            Console.WriteLine(euro2.Money);            
+        }
+        else if (result == 3)
+        {
+            Overloads();           
+        }
+        else
+        {
+            Console.WriteLine($"Выберите пункт из меню");            
+        }
+    }
+}
+GetMenu();
+void Overloads()
+{
+    Console.WriteLine($"Введите первое число");
+    string number1=Console.ReadLine();
+    bool check1 = int.TryParse(number1, out var result1);
+    Console.WriteLine($"Введите второе число");
+    string number2=Console.ReadLine();
+    bool check2=int.TryParse(number2, out var result2);
+    var a = new MyNumber { Value = result1 };
+    var b = new MyNumber { Value = result2 };
+    Console.WriteLine($"Сложение {a.Value + b.Value}");
+    Console.WriteLine($"Умножение{a.Value * b.Value}");
+    Console.WriteLine($"Вычетание{a.Value - b.Value}");
+    Console.WriteLine($"Деление{a.Value / b.Value}");
+}
